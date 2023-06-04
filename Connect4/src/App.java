16afe5e4f1947fdcc565f97c7e9c4188ff0449ab
AndresTorresPS <jaimetodu@hotmail.com>
2023-06-04 15:32:11 -0500

    Editing the hi

--- Connect4/src/App.java
+++ Connect4/src/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
                 System.out.println("Bye World!");
-                System.out.println("Hello AndresTorresPV!");
+                System.out.println("Hello AndresTorresPS!");
             }
         });
         
